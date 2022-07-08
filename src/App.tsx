8dd3c14db166cf646a69c2b6a3a48627d8d54fd1
Anastasia Videneeva <nastya.videneeva@gmail.com>
2022-07-07 22:29:46 -0700

    feat: add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
 import React from "react";
-import logo from "./logo.svg";
+import { ReactComponent as Icon } from "./icons/clear-sky.svg";
 import "./App.less";
+
+const CITIES = ["Ottawa", "Moscow", "Tokyo"];
+const DAYS = [
+  {
+    dayOfWeek: "Wed",
+    icon: Icon,
+    temperature: 18,
+  },
+  {
+    dayOfWeek: "Thu",
+    icon: Icon,
+    temperature: 19,
+  },
+  {
+    dayOfWeek: "Fri",
+    icon: Icon,
+    temperature: 20,
+  },
+  {
+    dayOfWeek: "Sat",
+    icon: Icon,
+    temperature: 21,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="cities">
+          {CITIES.map((city) => (
+            <button key={city} className="city">
+              {city.toUpperCase()}
+            </button>
+          ))}
+        </div>
+        <div className="weather">
+          <div className="today">
+            <span className="today-title">Today</span>
+            <div className="today-info">
+              <Icon className="today-icon" />
+              <div className="today-weather">
+                <span className="today-temperature">{`19°`}</span>
+                <span className="today-description">Clouds</span>
+              </div>
+            </div>
+          </div>
+          <div className="days">
+            {DAYS.map(({ dayOfWeek, icon: Icon, temperature }, index) => (
+              <div key={index} className="day">
+                <span className="day-week">{dayOfWeek}</span>
+                <Icon className="day-icon" />
+                <span className="day-temperature">{`${temperature}°`}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
