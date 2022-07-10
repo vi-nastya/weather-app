cab394d06c131cfa3b89bde6d15ffb07b5a003ad
Anastasia Videneeva <nastya.videneeva@gmail.com>
2022-07-09 21:42:00 -0700

    refactor: add global less variables

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,17 @@ module.exports = {
       plugin: require("craco-less"),
       options: {
         noIeCompat: true,
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@color-white": "#ffffff",
+              "@color-primary": "#333338",
+              "@color-primary-hover": "#5fb0e8",
+              "@color-secondary": "#edf6fa",
+            },
+            javascriptEnabled: true,
+          },
+        },
       },
     },
   ],
